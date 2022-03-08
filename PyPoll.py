ffe9d5df4d764c963bb08c1199bc3d0bce0f35f4
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-03-08 05:41:16 -0800

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -31,10 +31,20 @@ import csv
 
 #Straight from 3.4.2
 # Assign a variable for the file to load and the path.
-file_to_load = os.path.join("election_results.csv")
+file_to_load = os.path.join("test","election_analysis","election_results.csv")
 
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis","election_analysis.txt")
+
+#intiialize total vote counter
+total_votes = 0 #why place it here
+candidate_options = []
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 24
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -45,11 +55,47 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
 
     # Print each row in the CSV file.
-    #for row in file_reader:
-     #   print(row)
+    for row in file_reader:
+
+        candidate_name = row[2]
+        total_votes += 1
+
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+           # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0 
+        
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+    for candidate_name in candidate_votes:
+        
+        votes = candidate_votes[candidate_name]
+        vote_percentage = (float(votes) / float(total_votes))*100
+        print(f"{candidate_name}: received {vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+            winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+            
+    print(winning_candidate_summary)
+
+
+    
 
 #Output was suppose to be: <open file 'Resources/election_results.csv', mode 'r' at 0x10479c780>
 #Output I have at this step: <_io.TextIOWrapper name='election_results.csv' mode='r' encoding='cp1252'>
